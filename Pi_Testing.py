6615d3e69c62e5b3c3f7007b4296bc2f0ae92218
Levi J. Masters <42942430+Levijom@users.noreply.github.com>
2018-10-11 11:30:03 -0400

    Just some test stuff

---
+++ Pi_Testing.py
@@ -0,0 +1,35 @@
+"""
+n = float(input("num: "))
+
+a = int(n)
+
+c = n + 1
+
+d = c%n
+
+print(d)
+print(a)
+"""
+from decimal import *
+getcontext().prec = 25
+
+pi = 3.141592653589793238
+print(pi)
+
+import math
+print(math.pi)
+
+import decimal
+
+print(decimal.Decimal(math.pi))
+
+
+
+
+
+PI = 3 + ((4/(2*3*4)) - (4/(5*6*7)) + (4/(8+9+10)) - (4/(11*12*13)))
+print(PI)
+
+"""
+PI = 3 + loop n = n++,
+"""
